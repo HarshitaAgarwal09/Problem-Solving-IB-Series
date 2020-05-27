5a43e59ec41805e24caf97cd1a6a06231324f365
HarshitaAgarwal09 <harshitaagarwal98@yahoo.com>
2020-05-27 20:02:33 +0530

    Rotate Matrix O(n^2)

---
+++ Arrays/Rotate Matrix.cpp
@@ -0,0 +1,20 @@
+void Solution::rotate(vector<vector<int> > &A) {
+    for(int i=0;i<A.size();i++)
+    {
+        for(int j = i;j<A.size();j++)
+        {
+            swap(A[i][j],A[j][i]);        
+        }
+    }
+    
+    for(int i=0;i<A.size();i++)
+    {
+        int l = 0, r = A.size()-1;
+        while(l<r)
+        {
+            swap(A[i][l],A[i][r]);
+            l++;
+            r--;
+        }
+    }
+}
