75c99a82ddef410f636c27a7257da51bd5e9a130
HarshitaAgarwal09 <harshitaagarwal98@yahoo.com>
2020-05-27 17:59:21 +0530

    Maximum Unsorted SubArray

--- Arrays/Maximum Unsorted Subarray.cpp
+++ Arrays/Maximum Unsorted Subarray.cpp
@@ -1,19 +1,67 @@
 vector<int> Solution::subUnsort(vector<int> &A) 
 {
-    vector<int> B = A;
-    sort(B.begin(),B.end());
-    
     int l = -1;
     int r = -1;
     
-    for(int i = 0 ;i <A.size();i++)
+    for(int i = 0 ;i <A.size()-1;i++)
     {
-        if(A[i]!=B[i]){
-            r = i;
-            if(l == -1) l = i;
+        if(A[i]>A[i+1]) l = i;
+        if(l!=-1) break;
+    }
+    
+    
+    for(int i = A.size()-1 ;i >0; i--)
+    {
+        if(A[i]<A[i-1]) r = i;
+        if(r!=-1) break;
+    }
+    
+    
+    if(l==-1||r==-1) return {-1};
+    
+    int min_ = A[l];
+    int max_ = A[r];
+    
+    for(int i = l ; i<=r; i++)
+    {
+        min_ = min(min_,A[i]);
+        max_ = max(max_,A[i]);
+    }
+    
+    for(int i = 0 ;i<l ; i++){
+        if(A[i]>min_){
+            l=i;
+            break;
         }
     }
     
-    if(l==-1||r==-1) return {-1};
+    
+    for(int i = A.size()-1 ;i>r ; i--){
+        if(A[i]<max_){
+            r=i;
+            break;
+        }
+    }
+    
     return {l,r};
 }
+
+// vector<int> Solution::subUnsort(vector<int> &A) 
+// {
+//     vector<int> B = A;
+//     sort(B.begin(),B.end());
+    
+//     int l = -1;
+//     int r = -1;
+    
+//     for(int i = 0 ;i <A.size();i++)
+//     {
+//         if(A[i]!=B[i]){
+//             r = i;
+//             if(l == -1) l = i;
+//         }
+//     }
+    
+//     if(l==-1||r==-1) return {-1};
+//     return {l,r};
+// }
